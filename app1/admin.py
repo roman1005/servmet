2deca44e638202b654c68254f83e0f67f7fa1ad3
roman <2r.ivanets@gmail.com>
2020-06-02 10:55:16 +0300

    symplified admin added inlin for metricmeasurement added some other features

--- app1/admin.py
+++ app1/admin.py
@@ -1,24 +1,34 @@
 from django.contrib import admin
 from app1.models import Service, Staff, Metric, MetricMeasurement, MetricValue
 from simple_history.admin import SimpleHistoryAdmin
-from django.contrib.admin.views.main import ChangeList
-from django.core.paginator import EmptyPage, InvalidPage, Paginator
 
 
 class LinkedInline(admin.options.InlineModelAdmin):
-    template = "admin/edit_inline/tabular_paginator.html"
-    admin_model_path = None
+    template = "admin/edit_inline/inline_metrics.html"
+    #admin_model_path = None
 
+    '''
     def __init__(self, *args):
         super(LinkedInline, self).__init__(*args)
         if self.admin_model_path is None:
             self.admin_model_path = self.model.__name__.lower()
+    '''
 
 
 class MetricInline(LinkedInline):
     model = Metric
     extra = 0
-    fields = [ "metric_name"]
+    fields = ["metric_name"]
+    ordering = ('metric_name',)
+
+class MetricMeasurementInline(LinkedInline):
+    model = MetricMeasurement
+    extra = 0
+    fields = ['id']
+    readonly_fields = ['id']
+    ordering = ('-date_begin',)
+
+#class MetricMeasurementInLine()
 
 
 class ServiceAdmin(admin.ModelAdmin):
@@ -28,13 +38,22 @@ class ServiceAdmin(admin.ModelAdmin):
     search_fields = ('service_name', 'design_id')
     ordering = ('service_name',)
 
+
 class StaffAdmin(admin.ModelAdmin):
     ordering = ('name',)
     search_fields = ('name',)
 
+class MetricAdmin(admin.ModelAdmin):
+    ordering = ('metric_name',)
+    search_fields = ('metric_name',)
+    inlines = [
+        MetricMeasurementInline
+    ]
+
+
 admin.site.register(Service, ServiceAdmin)
 admin.site.register(Staff, StaffAdmin)
-admin.site.register(Metric, SimpleHistoryAdmin)
+admin.site.register(Metric, MetricAdmin)
 admin.site.register(MetricMeasurement, SimpleHistoryAdmin)
 admin.site.register(MetricValue, SimpleHistoryAdmin)
 # Register your models here.
